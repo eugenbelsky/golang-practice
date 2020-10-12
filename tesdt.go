5504c762e2015afbe48f8122c06dd0929220974b
KennyTheAnalBus <evgen.belsky@gmail.com>
2020-10-12 23:11:27 +0200

    12

---
+++ tesdt.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	// "strconv"
+	"strings"
+)
+
+func main() {
+	var inputStr = "192.168.1.1"
+	var a = strings.SplitAfter(inputStr, ".")
+	// var IPArray = [4]uint8{"192"}
+	for s := range a {
+		fmt.Printf(a[s])
+	}
+	// grades := [3]{13, 145, 54}
+	// fmt.Printf("Grades: %v", grades)
+
+}
+
+// func is_valid_ip(ip string) bool {
+//     var IPArray [4]uint8
+//     a :=
+// 	return false
+// }
