55f8424d974f2e989038da117dc9820e34f9a258
KennyTheAnalBus <evgen.belsky@gmail.com>
2020-10-15 12:09:48 +0200

    BitCounting v2

--- BitCounting/BitCounting.go
+++ BitCounting/BitCounting.go
@@ -3,19 +3,17 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	var inputINT uint = 1234
 	bitCount := CountBits(inputINT)
 	fmt.Printf("%v", bitCount)
+
 }
 
 func CountBits(v uint) int {
-	a, _ := strconv.Atoi(strconv.FormatUint(uint64(v), 2))
-	for i := range a {
-
-	}
-
-	return a
+	a := strconv.FormatUint(uint64(v), 2)
+	return strings.Count(a, strconv.Itoa(1))
 }
