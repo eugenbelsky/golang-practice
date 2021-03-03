54b1c5662493390d868e46cfeebb6f94f8f91b5a
Eugen Belsky <belskye@belskye.local>
2021-03-03 12:01:30 +0100

    codewars/Snail/main:

--- codewars/Snail/main.go
+++ codewars/Snail/main.go
@@ -4,75 +4,121 @@ import (
 	"fmt"
 )
 
+type Playground struct {
+	snaipMap [][]int
+}
+
 func main() {
 
-	playground := [][]int{
-		{1, 2, 3},
-		{4, 5, 6},
-		{7, 8, 9}}
-	output := Snail(playground)
+	initmap := [][]int{}
+	output := Snail(initmap)
 	fmt.Println(output)
 }
 func Snail(snaipMap [][]int) []int {
 
-	var output []int
+	output := []int{} // empty slice instead of nil
 
-	output = append(output, snaipMap[0]...) // to the right
-	snaipMap = snaipMap[1:]
+	var playground Playground
+	playground.snaipMap = snaipMap
 
-	for i := 0; i < len(snaipMap[0]); i++ { // right
-		output = append(output, snaipMap[0][i])
-		if len(snaipMap[0]) > 1 {
-			snaipMap[0] = snaipMap[0][1:]
-		} else {
-			snaipMap = snaipMap[1:]
-		}
-		i--
+	for len(playground.snaipMap) > 0 {
+		playground.Right(&output)
+		playground.Down(&output)
+		playground.Left(&output)
+		playground.Up(&output)
 	}
 
-	for i := 0; i < len(snaipMap); i++ { //down
-		output = append(output, snaipMap[i][len(snaipMap[i])-1])
-		if len(snaipMap[i]) > 1 {
-			snaipMap[i] = snaipMap[i][:len(snaipMap[i])-1]
-		} else {
-			if len(snaipMap) > 1 {
-				snaipMap = append(snaipMap[:i], snaipMap[:i+1]...)
-			} else {
-				return output
-			}
-		}
-
-	}
-
-	for i := len(snaipMap[len(snaipMap)-1]) - 1; i >= 0; i-- { // left
-		output = append(output, snaipMap[len(snaipMap)-1][i])
-		snaipMap[len(snaipMap)-1] = snaipMap[len(snaipMap)-1][:i-1]
-	}
-
-	for i := len(snaipMap) - 1; i >= 0; i-- { //up
-		output = append(output, snaipMap[i][0])
-		snaipMap[i] = snaipMap[i][0:]
-	}
-
-	fmt.Println(snaipMap)
-
-	// for i,v :=range snaipMap{
-	// 	ii,vv := range snaipMap[]{
-
-	// 	}
-	// }
 	return output
 }
 
-func removeElement(slice []int, i int) []int{
+func (playground *Playground) Up(output *[]int) {
+	if len(playground.snaipMap) == 0 {
+		return
+	}
+	for i := len(playground.snaipMap) - 1; i >= 0; i-- { //up
+		*output = append(*output, playground.snaipMap[i][0])
+
+		if len(playground.snaipMap[i]) > 1 {
+			playground.snaipMap[i] = removeElement(playground.snaipMap[i], 0)
+		} else {
+			playground.snaipMap = removeRaw(playground.snaipMap, i)
+		}
+	}
+	return
+}
+
+func (playground *Playground) Down(output *[]int) {
+
+	if len(playground.snaipMap) == 0 {
+		return
+	}
+	for i := 0; i < len(playground.snaipMap); i++ { //down
+		*output = append(*output, playground.snaipMap[i][len(playground.snaipMap[i])-1])
+
+		if len(playground.snaipMap[i]) > 1 {
+			playground.snaipMap[i] = removeElement(playground.snaipMap[i], len(playground.snaipMap[i])-1)
+		} else {
+			playground.snaipMap = removeRaw(playground.snaipMap, i)
+			i--
+		}
+
+	}
+	return
+}
+
+func (playground *Playground) Right(output *[]int) {
+
+	if len(playground.snaipMap) == 0 {
+		return
+	}
+	for i := 0; i < len(playground.snaipMap[0]); i++ { // right
+		*output = append(*output, playground.snaipMap[0][i])
+		playground.snaipMap[0] = removeElement(playground.snaipMap[0], i)
+		i--
+	}
+	if len(playground.snaipMap[0]) == 0 {
+		playground.snaipMap = removeRaw(playground.snaipMap, 0)
+	}
+	return
+}
+
+func (playground *Playground) Left(output *[]int) {
+
+	if len(playground.snaipMap) == 0 {
+		return
+	}
+	for i := len(playground.snaipMap[len(playground.snaipMap)-1]) - 1; i >= 0; i-- { // left
+		*output = append(*output, playground.snaipMap[len(playground.snaipMap)-1][i])
+		playground.snaipMap[len(playground.snaipMap)-1] = removeElement(playground.snaipMap[len(playground.snaipMap)-1], i)
+	}
+
+	if len(playground.snaipMap[len(playground.snaipMap)-1]) == 0 {
+		playground.snaipMap = removeRaw(playground.snaipMap, len(playground.snaipMap)-1)
+	}
+
+	return
+}
+
+func removeElement(slice []int, i int) []int {
 	if len(slice) > 0 {
 		if i == len(slice)-1 { //if last
 			slice = slice[:len(slice)-1]
 		} else {
-			slice = append(slice[:i], slice[i+1:]..)
+			slice = append(slice[:i], slice[i+1:]...)
 		}
 
-	} 
-	
+	}
+	return slice
+}
+
+func removeRaw(slice [][]int, i int) [][]int {
+	if len(slice) > 0 {
+		if i == len(slice)-1 { //if last
+			slice = slice[:len(slice)-1]
+		} else {
+			slice = append(slice[:i], slice[i+1:]...)
+		}
+
+	}
 	return slice
 }
